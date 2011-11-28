04037b3cf0b43c65a9ecdf0556ac84979b1121eb
adderly <adderlygonzalez@gmail.com>
2011-11-28 01:38:00 -0400

    Creation Of this

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<SDL/SDL.h>
+#include<SDL/SDL_image.h>
+#include<SDL/SDL_opengl.h>
+
+
+void init()
+{
+  if(SDL_Init(SDL_INIT_VIDEO) < 0){}
+  SDL_SetVideoMode(680,460,32,SDL_OPENGL);
+  
+  glMatrixMode();
+  glClearColor(1,1,1,1);  
+  glShadeModel(GL_SMOOTH);
+}
+
+int main(int argc, char* argv[])
+{
+  init(); 
+
+  bool isRunning = true;
+  SDL_Event event;
+  
+  while(isRunning)
+    {
+      while(SDL_PollEvent(&event))
+	{
+	  
+	}
+    }
+
+  return 0;
+}
+
+
+
---
+++ render.h
@@ -0,0 +1,10 @@
+
+class render
+{
+ public:
+  render();
+  int init();
+  void reshape(int width,int height);
+  static int setFullScreen();
+  void draw();
+};
