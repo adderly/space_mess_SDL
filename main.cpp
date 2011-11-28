e3730abec43aa5c5ef50108320c5d42ccee1133b
adderly <adderlygonzalez@gmail.com>
2011-11-28 05:35:30 -0400

    :D

--- main.cpp
+++ main.cpp
@@ -6,13 +6,29 @@
 
 void init()
 {
-  if(SDL_Init(SDL_INIT_VIDEO) < 0){}
+  if(SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout<<"Sorry!";
+		exit(1);
+	}
+  glClearColor(1,0,1,1);  
   SDL_SetVideoMode(680,460,32,SDL_OPENGL);
-  
-  glMatrixMode();
-  glClearColor(1,1,1,1);  
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(45,680.0/460.0,1.0,500.0);
+  glMatrixMode(GL_MODELVIEW);
   glShadeModel(GL_SMOOTH);
 }
+
+void Draw()
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glBegin(GL_TRIANGLES);
+			glVertex3f(0.0,2.0,-5.0);
+			glVertex3f(-2.0,-2.0,-5.0);
+			glVertex3f(2.0,-2.0,-5.0);
+		glEnd();
+	}
 
 int main(int argc, char* argv[])
 {
@@ -23,10 +39,14 @@ int main(int argc, char* argv[])
   
   while(isRunning)
     {
-      while(SDL_PollEvent(&event))
-	{
-	  
-	}
+	      while(SDL_PollEvent(&event))
+			{
+			  if(event.type == SDL_QUIT) isRunning = false;
+			  else if(event.key.keysym.sym = SDLK_ESCAPE) isRunning = false;
+			}
+			
+			draw();
+			SDL_GL_SwapBuffers();
     }
 
   return 0;
