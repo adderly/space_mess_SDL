fd99c865384a46e682bbe4ac2fee0f038b587990
adderly gonzalez <adderlygonzalez@gmail.com>
2011-11-28 06:15:09 -0300

    :D

--- main.cpp
+++ main.cpp
@@ -8,25 +8,25 @@ void init()
 {
   if(SDL_Init(SDL_INIT_VIDEO) < 0){}
   SDL_SetVideoMode(680,460,32,SDL_OPENGL);
-  
+
   glMatrixMode();
-  glClearColor(1,1,1,1);  
+  glClearColor(1,1,1,1);
   glShadeModel(GL_SMOOTH);
 }
 
 int main(int argc, char* argv[])
 {
-  init(); 
+  init();
 
   bool isRunning = true;
   SDL_Event event;
-  
+
   while(isRunning)
     {
-      while(SDL_PollEvent(&event))
-	{
-	  
-	}
+        while(SDL_PollEvent(&event))
+        {
+
+        }
     }
 
   return 0;
