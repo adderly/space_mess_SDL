8cfbe9915af7a3c76ed38ed0d24e28953276f60a
Richard Wardin <Shalmezad@gmail.com>
2012-02-25 19:48:40 -0500

    Fixed tabbing issues, modified window title text  for clarity

--- lib/Game.cpp
+++ lib/Game.cpp
@@ -60,8 +60,8 @@ void Game::MainLoop()
 
     while(Running)
     {
-      std::stringstream t;
-      std::string title;
+        std::stringstream t;
+        std::string title;
 
 
         movePlayers();
@@ -71,16 +71,15 @@ void Game::MainLoop()
         //std::cout<<SDL_GetError();
         //SDL_GL_SwapBuffers();
 
-
-
-        t << event.motion.x<<"   "<<event.motion.y <<"    "<< player->x<<"   "<<player->y;
+        t << "Event: (" << event.motion.x << "," << event.motion.y << ") ";
+        t << "Player: (" << player->x << "," << player->y << ")";
         title = t.str();
         SDL_WM_SetCaption(title.c_str(), 0);
 
-        if(1000/FPS>SDL_GetTicks()-start)
+        if(1000/FPS > SDL_GetTicks()-start)
                SDL_Delay(1000/FPS>SDL_GetTicks()-start);
     }
-
+    SDL_Quit();
 }
 void Game::checkEvents()
 {
@@ -88,122 +87,122 @@ void Game::checkEvents()
      {
         if(event.type == SDL_QUIT) Running = false;
 
-              if (event.type == SDL_MOUSEMOTION);
-                    //fallowMouse(event.motion.x,event.motion.y);
+        if (event.type == SDL_MOUSEMOTION);
+            //fallowMouse(event.motion.x,event.motion.y);
 
-              if(event.type == SDL_MOUSEBUTTONDOWN)
-              {
-                  switch(event.button.button)
-                  {
-                      case SDL_BUTTON_LEFT:
+        if(event.type == SDL_MOUSEBUTTONDOWN)
+        {
+            switch(event.button.button)
+            {
+                case SDL_BUTTON_LEFT:
                       MOUSE_LCLICK = true;
                       //shut();
                       break;
-                      case SDL_BUTTON_RIGHT:
+                case SDL_BUTTON_RIGHT:
                        music->playEffect("sdfsd");
                       MOUSE_RCLICK = true;
                       break;
-                      case SDL_BUTTON_MIDDLE:
+                case SDL_BUTTON_MIDDLE:
                       MOUSE_MCLICK = true;
                       break;
-                      case SDL_BUTTON_X1:
+                case SDL_BUTTON_X1:
                       break;
-                      case SDL_BUTTON_X2:
+                case SDL_BUTTON_X2:
                       break;
-                      default:
+                default:
                       break;
-                  }
-              }
-              if(event.type == SDL_MOUSEBUTTONUP)
-              {
-                  switch(event.button.button)
-                  {
-                      case SDL_BUTTON_LEFT:
-                      MOUSE_LCLICK = false;
-                      //shut();
-                      break;
-                      case SDL_BUTTON_RIGHT:
-                      MOUSE_RCLICK = false;
-                      break;
-                      case SDL_BUTTON_MIDDLE:
-                      MOUSE_MCLICK = false;
-                      break;
-                      case SDL_BUTTON_X1:
-                      break;
-                      case SDL_BUTTON_X2:
-                      break;
-                      default:
-                      break;
-                  }
-              }
-
-
-            if(event.type == SDL_KEYDOWN)
+            }
+        }
+        if(event.type == SDL_MOUSEBUTTONUP)
+        {
+            switch(event.button.button)
             {
-                switch(event.key.keysym.sym)
-                {
-                    case SDLK_RIGHT:
-                        p_right = true;
+                case SDL_BUTTON_LEFT:
+                    MOUSE_LCLICK = false;
+                    //shut();
                     break;
-                    case SDLK_LEFT:
-                        p_left = true;
+                case SDL_BUTTON_RIGHT:
+                    MOUSE_RCLICK = false;
                     break;
-                    case SDLK_UP:
-                        p_up = true;
+                case SDL_BUTTON_MIDDLE:
+                    MOUSE_MCLICK = false;
                     break;
-                    case SDLK_DOWN:
-                        p_down = true;
+                case SDL_BUTTON_X1:
                     break;
-                    case SDLK_p:
+                case SDL_BUTTON_X2:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+        if(event.type == SDL_KEYDOWN)
+        {
+            switch(event.key.keysym.sym)
+            {
+                case SDLK_RIGHT:
+                    p_right = true;
+                    break;
+                case SDLK_LEFT:
+                    p_left = true;
+                    break;
+                case SDLK_UP:
+                    p_up = true;
+                    break;
+                case SDLK_DOWN:
+                    p_down = true;
+                    break;
+                case SDLK_p:
                     music->play();
                     break;
-                    case SDLK_r:
+                case SDLK_r:
                     music->resume();
                     break;
-                    case SDLK_s:
+                case SDLK_s:
                     music->pause();
                     break;
-                    default:
+                default:
                     break;
-                }
             }
-            if(event.type == SDL_KEYUP)
+        }
+        if(event.type == SDL_KEYUP)
+        {
+            switch(event.key.keysym.sym)
             {
-                switch(event.key.keysym.sym)
-                {
-                     case SDLK_RIGHT:
-                        p_right = false;
+                case SDLK_RIGHT:
+                    p_right = false;
                     break;
-                    case SDLK_LEFT:
-                        p_left = false;
+                case SDLK_LEFT:
+                    p_left = false;
                     break;
-                    case SDLK_UP:
-                        p_up = false;
+                case SDLK_UP:
+                    p_up = false;
                     break;
-                    case SDLK_DOWN:
-                        p_down = false;
+                case SDLK_DOWN:
+                    p_down = false;
                     break;
-                    case SDLK_ESCAPE:
-                        Running = false;
+                case SDLK_ESCAPE:
+                    Running = false;
                     break;
-                    case SDLK_F12:
-                        //rend.setFullScreen();
+                case SDLK_F12:
+                    //rend.setFullScreen();
                     break;
-                    default:
+                default:
                     break;
-                }
             }
+        }
      }
 
 }
 void Game::setTitle(int w,int h)
 {
     std::stringstream t2;
-      std::string title2;
+    std::string title2;
 
-          t2 << x<<"   "<<y;
-        title2 = t2.str();
-           SDL_WM_SetCaption(title2.c_str(), 0);
+    t2 << x<<"   "<<y;
+    title2 = t2.str();
+    SDL_WM_SetCaption(title2.c_str(), 0);
 }
 Game::~Game()
 {
